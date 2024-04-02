eaba90445e517777b642b01b0d19c7cd89e28b06
suhas-sunder <suhas@live.ca>
2024-04-02 18:03:43 -0400

    feat: game logic
    -Add logic to complete memory game so that when user selects two of the same letter, it gets highlighted in green & becomes non-selectable. If the user selects two wrong options, the revealed letters reset for squares that are not a match.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//There is an edge case I need to fix where clicking on the same letter/option twice registers as a win.
 const memoryArr = [
   "A",
   "A",
@@ -17,6 +18,9 @@ const memoryArr = [
   "H",
 ];
 
+let clickCount = 0;
+let lastLetter = "";
+
 const shuffled = memoryArr
   .map((value) => ({ value, sort: Math.random() }))
   .sort((a, b) => a.sort - b.sort)
@@ -25,7 +29,40 @@ const shuffled = memoryArr
 const squares = document.getElementById("squares");
 
 const handleClick = (e) => {
-  e.target.style.color === "white" ?  e.target.style.color = "black" : e.target.style.color = "white";
+  e.target.classList.contains("square") && e.target.style.color === "white"
+    ? (e.target.style.color = "black")
+    : (e.target.style.color = "white");
+
+  const allSquares = document.getElementsByClassName("square");
+
+  const lastMatchingSquare = Array.from(allSquares).filter(
+    (square) =>
+      square.textContent === e.target.textContent && square !== e.target
+  )[0];
+
+  if (clickCount % 2 === 0) {
+    Array.from(allSquares).forEach((square) => {
+      if (square !== e.target) {
+        square.style.color = "black";
+      }
+    });
+  }
+  clickCount++;
+
+  //Disables matching buttons or resets unmatched btns
+  if (clickCount % 2 === 0 && lastLetter === e.target.textContent) {
+    e.target.classList.remove("square");
+    e.target.classList.add("completed");
+    e.target.style.color = "black";
+    e.target.disabled = true;
+
+    lastMatchingSquare.classList.remove("square");
+    lastMatchingSquare.classList.add("completed");
+    lastMatchingSquare.style.color = "black";
+    lastMatchingSquare.disabled = true;
+  }
+
+  lastLetter = e.target.textContent;
 };
 
 shuffled.forEach((square) => {
